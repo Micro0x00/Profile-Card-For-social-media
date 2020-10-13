bed778957d65277b477444442788395d189ba92f
Micro <67539414+Micro0x00@users.noreply.github.com>
2020-10-13 18:51:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var messageBox = document.querySelector('.js-message');
   var btn = document.querySelector('.js-message-btn');
   var card = document.querySelector('.js-profile-card');
   var closeBtn = document.querySelectorAll('.js-message-close');
-
+// Micro
   btn.addEventListener('click',function (e) {
       e.preventDefault();
       card.classList.add('active');
