dccd407c8fb8a8db18e6a03c16097c1e17ae6f49
Micro <67539414+Micro0x00@users.noreply.github.com>
2021-03-05 10:13:14 +0200

    test

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var messageBox = document.querySelector('.js-message');
-  var btn = document.querySelector('.js-message-btn');
-  var card = document.querySelector('.js-profile-card');
-  var closeBtn = document.querySelectorAll('.js-message-close');
-// Micro
-  btn.addEventListener('click',function (e) {
-      e.preventDefault();
-      card.classList.add('active');
-  });
+// var messageBox = document.querySelector('.js-message');
+//   var btn = document.querySelector('.js-message-btn');
+//   var card = document.querySelector('.js-profile-card');
+//   var closeBtn = document.querySelectorAll('.js-message-close');
+// // Micro
+//   btn.addEventListener('click',function (e) {
+//       e.preventDefault();
+//       card.classList.add('active');
+//   });
 
-  closeBtn.forEach(function (element, index) {
-     console.log(element);
-      element.addEventListener('click',function (e) {
-          e.preventDefault();
-          card.classList.remove('active');
-      });
-  });
+//   closeBtn.forEach(function (element, index) {
+//      console.log(element);
+//       element.addEventListener('click',function (e) {
+//           e.preventDefault();
+//           card.classList.remove('active');
+//       });
+//   });
