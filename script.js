4e1f0f91f9df148c2b4c0712a6a65c081b8e7709
Muhammad Mater <67539414+Micro0x00@users.noreply.github.com>
2022-10-31 20:41:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-// var messageBox = document.querySelector('.js-message');
-//   var btn = document.querySelector('.js-message-btn');
-//   var card = document.querySelector('.js-profile-card');
-//   var closeBtn = document.querySelectorAll('.js-message-close');
-// // Micro
-//   btn.addEventListener('click',function (e) {
-//       e.preventDefault();
-//       card.classList.add('active');
-//   });
-
-//   closeBtn.forEach(function (element, index) {
-//      console.log(element);
-//       element.addEventListener('click',function (e) {
-//           e.preventDefault();
-//           card.classList.remove('active');
-//       });
-//   });
+$(function()
+{
+    // Get Number of Twitter Followers From Twitter.com
+    twitterusername = 'hputtick';
+    $.getJSON('http://twitter.com/users/'+twitterusername+'.json?callback=?',
+    function(data)
+    {
+        $('#twitflw').html('Twitter Followers : ' + data.followers_count + '');
+    });
+})
